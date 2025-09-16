fad0a46529d7e783d7c0039bb3d1ac75d48d0b79
Matheus de Souza <matheusgds3011@gmail.com>
2025-09-16 15:45:27 -0300

    Pegando valores importantes para a manipulação dos elementos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 function adicionar(){
-
+    let produto = document.getElementById("produto").value;
+    let nomeProduto = produto.split("-")[0];
+    let valorProduto = produto.split("R$")[1];
+    let quantidade = document.getElementById("quantidade").value;
 }
 
 function limpar(){
