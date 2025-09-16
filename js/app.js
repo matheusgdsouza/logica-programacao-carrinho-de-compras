aac7564c4c29f1914f482f9c416f8bc3f8367f2c
Matheus de Souza <matheusgds3011@gmail.com>
2025-09-16 15:55:59 -0300

    Adicionando produtos a lista de compras, limpando o campo de quantidade após adição do produto, adicionando comentários para facilitar a leitura do código

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 function adicionar(){
+    //Pegando valores 
     let produto = document.getElementById("produto").value;
     let nomeProduto = produto.split("-")[0];
     let valorProduto = produto.split("R$")[1];
     let quantidade = document.getElementById("quantidade").value;
+    //Adicionando produtos selecionados a lista
+    let listaProdutos = document.getElementById("lista-produtos");
+    listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>
+        </section>`;
+    //Limpando o valor de quantidade para não gerar confusão
+    document.getElementById("quantidade").value = " ";
 }
 
 function limpar(){
+    //Limpando lista de produtos
     document.getElementById("lista-produtos").innerHTML = "";
+    //Zerando valor total da compra
     document.getElementById("valor-total").textContent = "R$0,00";
 }
